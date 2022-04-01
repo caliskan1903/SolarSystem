5983618290df1ed2a4f6ba17f43e6eea5b8e08d4
gokhanc <gokuclskn@gmail.com>
2022-04-01 21:10:07 +0100

    The Basic Structure is complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         SolarSystem milkyway=new SolarSystem();
-
-
-
+        milkyway.sun=1;
+        milkyway.Moon=2;
+        milkyway.Planets=3;
 
     }
 }
